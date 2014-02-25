972ea5956c1df4ff08607041ec17be73a547447a
liuenze <liu.enze@me.com>
2014-02-25 10:51:42 +0800

    add friday

---
+++ friday/friday.cpp
@@ -0,0 +1,17 @@
+/*
+ID: liu.enz1 
+PROG: friday
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("friday.out");
+    ifstream fin ("friday.in");
+    return 0;
+}
+

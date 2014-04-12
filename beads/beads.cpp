48422c5447cbd152b11800d7a15b8df53ad5e2d7
liuenze <liu.enze@me.com>
2014-04-12 17:38:15 +0800

    fin beads

--- beads/beads.cpp
+++ beads/beads.cpp
@@ -8,40 +8,70 @@ LANG: C++
 #include <string>
 
 using namespace std;
-int max(int* counts)
+
+int equal(char a ,char b)
 {
-  return counts[0];
+  return a==b||a=='w'||b=='w';
 }
 
-int beads(int n,const char* necklace)
+int max (int * res, int len)
 {
-  int* counts = new int[n];
-  int index = 0;
-  char last = 0;
-  int count = 1;
-
-  for(int i=0;i<n;i++)
+  if (len ==1)
+    return res[0];
+  int max=0,last = res[0];
+    cout<<res[0]<<" ";
+  for(int i=1;i<len;i++)
   {
-    if(necklace[i]==last || necklace[i]=='w')
+    cout<<res[i]<<" ";
+    if(res[i-1]<0)
+      last = res[i]-res[i-1];
+    else
+    {
+      int tmp = last+(res[i]<0?res[i+1]:res[i]);
+      last = res[i];
+      max = max>tmp?max:tmp;
+    }
+  }
+  return max;
+}
+
+
+int beads(int num,const char* necklace)
+{
+  int count=0;
+  char last = necklace[0],begin=necklace[0],end=necklace[0];
+  int index = 0,wnum = 0;
+  int* res = new int[num+1];
+  for(int i = 0;i<num;i++)
+  {
+    if(equal(last,necklace[i]))
     {
       count++;
-      continue;
+      if(last =='w' && necklace[i]!='w') last = necklace[i];
+      if(begin == 'w'&& necklace[i]!='w') begin = necklace[i];
+      if(necklace[i]=='w') wnum++;else wnum = 0;
     }
-
-    last = necklace[i];
-    counts[index]= count;
-    index = i;
-    count = 1;
+    else
+    {
+      res[index++]= count;
+      if(wnum != 0) res[index++]= -wnum;
+      last = necklace[i];
+      if(last =='r'||last == 'b') end = last;
+      count = 1;
+      wnum = 0;
+    }
   }
-  if(counts[n-1]==0&& (necklace[n-1]==necklace[0]||necklace[0]=='w'))
-  {
-    counts[index]+=counts[0];
+  if(equal(last,necklace[num-1])) res[index++]=count;
+  if(index >=2){
+  if(begin == end) {res[0]=res[index-1]+res[0]+(res[index-2]<0?-res[index-2]:0);res[index-1]=res[0]+(res[index-2]<0?res[index-2]:0);}
+  else res[0]+=wnum;}
+  return max(res, index);
+}
 
-  }
-
-  int beads = max(counts);
-  delete[] counts;
-  return beads;
+int max(int a ,int b)
+{
+  if (a >b) return a;
+  return b;
 }
 
 int main() {
@@ -53,4 +83,3 @@ int main() {
     fout <<beads(a,b.c_str()) << endl;
     return 0;
 }
-

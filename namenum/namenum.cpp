bde7d27b3a84bba34d20bf9f232b5242dbdbd6da
liuenze <liu.enze@me.com>
2014-08-05 15:06:14 +0800

    add transform

---
+++ namenum/namenum.cpp
@@ -0,0 +1,23 @@
+/*
+ID: liu.enz1
+PROG: transform
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int
+main(void)
+{
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    int a, b;
+    fin >> a >> b;
+    fout << a+b << endl;
+    return 0;
+}
+

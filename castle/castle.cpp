a94917a64649837454d6e5a1711a841979f8c565
liuenze <liu.enze@me.com>
2014-08-25 23:19:33 +0800

    fix level 1, go to level 2

---
+++ castle/castle.cpp
@@ -0,0 +1,17 @@
+/*
+ID: liu.enz1
+PROG: castle
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ofstream fout ("castle.out");
+ifstream fin ("castle.in");
+
+int main() {
+  return 0;
+}

21ef1a6b29fa3144805c808e40fb1ccfbc7a5a96
liuenze <liu.enze@me.com>
2014-09-05 22:04:11 +0800

    finish holstein

--- holstein/holstein.cpp
+++ holstein/holstein.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <queue>
 #include <stdlib.h>
 
 using namespace std;
@@ -17,27 +18,72 @@ ifstream fin ("holstein.in");
 #define MAX_FEED 15
 
 int vitmains;
-int requriements[MAX_VIT] = {0};
 int feedTypes;
 int feeds[MAX_FEED][MAX_VIT] = {{0}};
+
+typedef struct{
+  int requirements[MAX_VIT];
+  int result[MAX_FEED];
+  int hasFeed;
+  int feedNum;
+} State;
+
+State init;
+queue<State> stateList;
+State result;
+bool hasSolved = false;
 
 void
 input(){
   fin>>vitmains;
   for(int i=0;i<vitmains;i++)
-    fin>>requriements[i];
+    fin>>init.requirements[i];
   fin>>feedTypes;
   for(int i=0;i<feedTypes;i++)
     for(int j=0;j<vitmains;j++)
       fin>>feeds[i][j];
+  init.hasFeed = -1;
+  stateList.push(init);
 }
 
 void
 output(){
+  fout<<result.feedNum;
+  for(int i=0;i<result.feedNum;i++)
+    fout<<" "<<result.result[i]+1;
+  fout<<endl;
+}
+
+void trySolve(State state, int feedType){
+  for(int i=0;i<MAX_VIT;i++)
+    state.requirements[i]-=feeds[feedType][i];
+
+  state.result[state.feedNum++] = feedType;
+  state.hasFeed = feedType;
+
+  bool isSolved = true;
+  for(int i=0;i<vitmains;i++) {
+    if(state.requirements[i]>0)
+      isSolved = false;
+  }
+  hasSolved = isSolved;
+  if(isSolved == true){
+    result = state;
+  }
+  else
+    stateList.push(state);
 }
 
 void
 solve(){
+  while(!stateList.empty()){
+    State cur = stateList.front();
+    stateList.pop();
+    for(int i=0;i<feedTypes;i++){
+      if(hasSolved) return;
+      if(cur.hasFeed<i)
+        trySolve(cur,i);
+    }}
 }
 
 int main() {

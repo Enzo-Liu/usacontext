6fe5788ef0c79640167a14a57cb7ebf1c0674fc1
liuenze <liu.enze@me.com>
2014-09-12 10:14:52 +0800

    add sample for init usacontext program

---
+++ sample/sample.cpp
@@ -0,0 +1,36 @@
+/*
+  ID: liu.enz1
+  PROG: #{name}
+  LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <set>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+ofstream fout ("#{name}.out");
+ifstream fin ("#{name}.in");
+
+void
+input(){
+}
+
+void
+output(){
+}
+
+void
+solve(){
+}
+
+int main() {
+  input();
+  solve();
+  output();
+  return 0;
+}

69bca4dfdbb21cb62e6405670aa03bf8174da4ed
liuenze <liu.enze@me.com>
2014-10-16 18:06:25 +0800

    init wormhole

---
+++ wormhole/wormhole.cpp
@@ -0,0 +1,36 @@
+/*
+  ID: liu.enz1
+  PROG: wormhole
+  LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <set>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+ofstream fout ("wormhole.out");
+ifstream fin ("wormhole.in");
+
+void
+input(){
+}
+
+void
+output(){
+}
+
+void
+solve(){
+}
+
+int main() {
+  input();
+  solve();
+  output();
+  return 0;
+}

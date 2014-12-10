8a9949dfca5505a7128ea653235f8f4d45e83ac3
liuenze <liu.enze@me.com>
2014-12-10 21:07:39 +0800

    update spin partially

---
+++ spin/spin.cpp
@@ -0,0 +1,36 @@
+/*
+  ID: liu.enz1
+  PROG: spin
+  LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <set>
+#include <stdlib.h>
+#include <math.h>
+
+using namespace std;
+
+ofstream fout ("spin.out");
+ifstream fin ("spin.in");
+
+void
+input(){
+}
+
+void
+output(){
+}
+
+void
+solve(){
+}
+
+int main() {
+  input();
+  solve();
+  output();
+  return 0;
+}
